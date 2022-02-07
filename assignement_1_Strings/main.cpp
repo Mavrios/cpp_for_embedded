23a498e46533589a5f5778622772b6ef8eb73909
Mavrios <ahmedadelkishk@gmail.com>
2022-02-07 14:13:18 +0200

    Documenting Assignment_1_String

--- assignement_1_Strings/main.cpp
+++ assignement_1_Strings/main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 #define NUM_CATOGORY    4
 #define NUM_EXTENSIONS   9
 
-
+/*Enum for Extension Categories*/
 typedef enum{
     Music = 0,
     Image,
@@ -17,12 +17,13 @@ typedef enum{
     Others
 }Files;
 
+/*Function Prototypes*/
 Files Check_Extension(string str);
 string SizeDetector(string str);
 
 int main(void)
 {
-    
+    /*Initialization Variables*/
     uint32_t LOC_u32NumTestCases = 0;
     Files LOC_u8_Extension;
     uint32_t LOC_u32Iterator1 = 0, LOC_u32Iterator2 = 0;
@@ -31,83 +32,85 @@ int main(void)
     uint32_t LOC_u32Size = 0;
     uint32_t Result[NUM_CATOGORY];
     uint32_t * LOC_u32PtrTestCases = NULL;
+
+    /*SCAN NUMBER OF TEST CASES*/
     cin >> LOC_u32NumTestCases;
+    
+    /*DECLARE ARRAY WITH SIZE OF NUMBER OF TEST-CASES */
     LOC_u32PtrTestCases = new uint32_t[LOC_u32NumTestCases];
+    /*LOOP TO SCAN EACH NUMBER OF INPUT OF EACH TEST CASES*/
     for (LOC_u32Iterator1 = 0; LOC_u32Iterator1 < LOC_u32NumTestCases; LOC_u32Iterator1++)
     {
         cin>>LOC_u32PtrTestCases[LOC_u32Iterator1];
     }
-
+    /*LOOP TO RUN EACH TEST CASE*/
     for (LOC_u32Iterator1 = 0; LOC_u32Iterator1 < LOC_u32NumTestCases; LOC_u32Iterator1++)
     {
+        /*RESET RESULT ARRAY*/
         memset(Result,0,sizeof(Result));
+        /*LOOP TO TEST EACH INPUT IN TEST CASES*/
         for(LOC_u32Iterator2 = 0 ; LOC_u32Iterator2 <LOC_u32PtrTestCases[LOC_u32Iterator1] ; LOC_u32Iterator2++)
         {
+            /*READ TOTAL LINE FROM CMD*/
             getline(cin >> ws, LOC_TempStringName);
+            /*GET THE SIZE FROM THE END OF STRING*/
             LOC_TempStringSize=SizeDetector(LOC_TempStringName);
+            /*REMOVE THE SIZE FROM THE STRING*/
             LOC_TempStringName.erase(LOC_TempStringName.find(" "+LOC_TempStringSize),LOC_TempStringSize.length()+1);
-
+            /*STRING STREAM TO GET INT FROM STRING*/
             stringstream TempSS(LOC_TempStringSize.substr(0,LOC_TempStringSize.length()-1));
             TempSS>>LOC_u32Size;
+            /*CALL FUNCTION TO CHECK EXTENSION FROM FILE NAME AND RETURN EXTENSION(ENUM)*/
             LOC_u8_Extension = Check_Extension(LOC_TempStringName);
+            /*ACCESS ARRAY EXTENSION INDEX AND STORE DETECTED SIZE*/
             Result[LOC_u8_Extension]+=LOC_u32Size;
         }
+        /*PRINT THE OUTPUT*/
     cout<<"music "<<Result[Music]<<"b "<<"images "<<Result[Image]<<"b "<<"movies "<<Result[Movie]<<"b "<<"other "<<Result[Others]<<"b"<<endl;
     }
+    /*FREE RESERVED SPACE FROM HEAP*/
+    delete(LOC_u32PtrTestCases);
+    return 0;
 }
 
-//mysong.mp3.jpg   rfind
+/**
+ * Extension Detector From String
+ *
+ * @param str Container of string represent file name.
+ * @return Enum Extension of file name `Result`.
+ */
 Files Check_Extension(string str)
 {
-    Files Result = Music;
+    /*VARIABLE WHOSE CONTAINING RETURNED EXTENSION*/
+    Files Result = Others;
+    /*CHECK BY REGEX IF THE 'str' ENDS WITH ONE OF MUSIC EXTENSIONS*/
     if(regex_match(str, regex("^.*(?=aac$|mp3$|flac$)[^.]+$")))
         Result = Music;
+    /*CHECK BY REGEX IF THE 'str' ENDS WITH ONE OF IMAGE EXTENSIONS*/
     else if(regex_match(str, regex("^.*(?=jpg$|gif$|bmp$)[^.]+$")))
         Result = Image;
+    /*CHECK BY REGEX IF THE 'str' ENDS WITH ONE OF MOVIE EXTENSIONS*/
     else if(regex_match(str, regex("^.*(?=mp4$|avi$|mkv$)[^.]+$")))
         Result = Movie;
-    else
-    {
-        Result = Others;
-    }
-    // string LOC_StrExtenstions[9]={".mp3",".aac",".flac" ,".jpg",".gif",".bmp",".mp4",".avi",".mkv"};
-    // uint8_t u8Index = 0;
-    // uint8_t LOC_Iterator = 0;
-    // //Song.aac.jpg.mp3
-    // for(LOC_Iterator = 0 ; LOC_Iterator < NUM_EXTENSIONS ; LOC_Iterator++)
-    // {
-    //     if((LOC_Iterator>=0 && LOC_Iterator < 3) && (u8Index < str.rfind(LOC_StrExtenstions[LOC_Iterator]) &&  str.rfind(LOC_StrExtenstions[LOC_Iterator]) <100 ))
-    //     {
-    //         Result = Music;
-    //         u8Index = str.rfind(LOC_StrExtenstions[LOC_Iterator]);
-    //         // cout<<"MUSIC"<<endl;
-    //     }
-    //     if((LOC_Iterator>=3 && LOC_Iterator < 6) && (u8Index < str.rfind(LOC_StrExtenstions[LOC_Iterator]) &&  str.rfind(LOC_StrExtenstions[LOC_Iterator]) <100 ))
-    //     {
-    //         Result = Image;
-    //         u8Index = str.rfind(LOC_StrExtenstions[LOC_Iterator]);
-    //         // cout<<"IMAGE"<<endl;
-    //     }
-    //     if((LOC_Iterator>=6 && LOC_Iterator <= NUM_EXTENSIONS)  && (u8Index < str.rfind(LOC_StrExtenstions[LOC_Iterator]) &&  str.rfind(LOC_StrExtenstions[LOC_Iterator]) <100 ))
-    //     {
-    //         Result = Movie;
-    //         u8Index = str.rfind(LOC_StrExtenstions[LOC_Iterator]);
-    //         // cout<<"MOVIE"<<endl;
-    //     }
-    // }
-    // if(u8Index < str.rfind(".") && str.rfind(".") < 100)
-    // {
-    //     Result = Others;
-    //     // cout<<"OTHERS"<<endl;
-    // }
 
+    /*RETURN RESULT*/
     return Result;
     }
 
+/**
+ * Size Detector From String
+ *
+ * @param str Container of string represent file name with the file size in bytes.
+ * @return String of file size `temp`.
+ */
 string SizeDetector(string str)
 {
+    /*INSTANCE OF STRINGSTREAM AND CALLING CONSTRUCTOR OF 1 PARAMETER*/
     stringstream ss(str);
+    /*TEMP VARIABLE*/
     string temp = "";
+    /*KEEP SPLITING THE STRING WITH WHITESPACES KEY UNTIL REACH TO THE LAST STRING*/
     while (ss>>temp);
+    /*RETURN LAST STRING WHICH IS SIZE OF FILE*/
     return temp;
 }
